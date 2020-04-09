d13b8bd86276115064b29a0e39254162bf90eff3
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-04-09 11:43:57 +0900

    added a simple query

--- src/index.js
+++ src/index.js
@@ -1,19 +1,43 @@
 const { GraphQLServer } = require("graphql-yoga");
 
+// Schema
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
+// Dummy data
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
+
+// Resolver function
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
-}
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  // Link: {
+  //   id: (parent) => parent.id,
+  //   description: (parent) => parent.description,
+  //   url: (parent) => parent.url,
+  // },
+};
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
 });
-server.start(() => console.log(`Server is runnning on http://localhost:4000`))
+server.start(() => console.log(`Server is runnning on http://localhost:4000`));
