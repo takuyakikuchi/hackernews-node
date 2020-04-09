d1f3e34c10ca1aee9233698aaf5182d60633e2f5
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-04-09 15:58:33 +0900

    added subscription

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ const { GraphQLServer } = require("graphql-yoga");
 const { prisma } = require("./generated/prisma-client");
 const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
+const Subscription = require("./resolvers/Subscription");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
+const Vote = require("./resolvers/Vote");
 
 // Resolver function
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link,
+  Vote,
 };
 
 const server = new GraphQLServer({
